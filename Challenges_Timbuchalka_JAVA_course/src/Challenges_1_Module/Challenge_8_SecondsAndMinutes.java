a60d5e84400daecc8007e5c488b5f2caaafaa7f3
gocool94 <gocool94@gmail.com>
2022-06-27 22:25:13 +0530

    Challenged

---
+++ Challenges_Timbuchalka_JAVA_course/src/Challenges_1_Module/Challenge_8_SecondsAndMinutes.java
@@ -0,0 +1,18 @@
+package Challenges_1_Module;
+
+public class Challenge_8_SecondsAndMinutes {
+    public static void main(String[] args){
+
+
+    }
+
+
+    public static void getDurationString(int Minutes, int Seconds){
+
+        if (Minutes >= 0 && Seconds>= 0 && Seconds <=59 ){
+
+        }
+    }
+
+
+}
