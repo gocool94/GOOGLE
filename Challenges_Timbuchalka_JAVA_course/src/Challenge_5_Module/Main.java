7d2a809fa3eb8f7dffa7e014826999329a6151b0
gocool94 <gocool94@gmail.com>
2022-06-27 18:35:49 +0530

    Hello

--- Challenges_Timbuchalka_JAVA_course/src/Challenge_5_Module/Main.java
+++
@@ -1,9 +0,0 @@
-package Challenge_5_Module;
-
-public class Main {
-
-    public static void main(String[] args){
-
-        System.out.println("Hello Gokul");
-    }
-}
--- Challenges_Timbuchalka_JAVA_course/src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-
-    public static void main(String[] args){
-
-        car maruthi = new car();
-        maruthi.setModelName("Ciaz");
-        System.out.println(maruthi.Check_number_of_tyres("ciaz"));
-    }
-}
